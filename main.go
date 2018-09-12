fbb0fe06bd8e225c86bae82d54f8ea923da12751
Gregor Noczinski <gregor@noczinski.eu>
2018-09-12 16:18:45 +0200

    Improve demo

--- main.go
+++ main.go
@@ -16,13 +16,19 @@ func getElementById(id string) js.Value {
 func renderEditor(parent js.Value) js.Value {
 	editorMarkup := `
 		<div id="editor" style="display: flex; flex-flow: row wrap;">
-			<textarea id="markdown" style="width: 50%; height: 400px"></textarea>
+			<textarea id="markdown" style="width: 50%; height: 400px">This *is* a **test**.</textarea>
 			<div id="preview" style="width: 50%;"></div>
 			<button id="render">Render Markdown</button>
 		</div>
 	`
 	parent.Call("insertAdjacentHTML", "beforeend", editorMarkup)
 	return getElementById("editor")
+}
+
+func renderMarkdown(markdown js.Value, preview js.Value) {
+	md := markdown.Get("value").String()
+	html := github_flavored_markdown.Markdown([]byte(md))
+	preview.Set("innerHTML", string(html))
 }
 
 func main() {
@@ -33,11 +39,9 @@ func main() {
 	preview := getElementById("preview")
 	renderButton := getElementById("render")
 	renderButton.Set("onclick", js.NewCallback(func([]js.Value) {
-		// Process markdown input and show the result
-		md := markdown.Get("value").String()
-		html := github_flavored_markdown.Markdown([]byte(md))
-		preview.Set("innerHTML", string(html))
+		renderMarkdown(markdown, preview)
 	}))
+	renderMarkdown(markdown, preview)
 
 	<-done
 }
